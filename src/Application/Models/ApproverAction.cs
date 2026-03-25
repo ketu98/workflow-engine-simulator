2dd6fa9629470808be0dd4563c40f75ba647365c
ANIKET GHOSH <aniketghosh98@gmail.com>
2026-03-25 11:01:06 +0530

    Add ApproverAction model with properties

--- src/Application/Models/ApproverAction.cs
+++ src/Application/Models/ApproverAction.cs
@@ -1,1 +1,10 @@
+namespace WorkflowEngineSimulator.Models;
 
+public class ApproverAction
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string ApproverEmail { get; set; } = string.Empty;
+    public ApprovalDecision? Decision { get; set; }
+    public DateTime? ActionedOnUtc { get; set; }
+    public string? Comments { get; set; }
+}
