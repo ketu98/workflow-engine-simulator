926d7080cc827cbdc068f8c9d448e5f578de2f4e
ANIKET GHOSH <aniketghosh98@gmail.com>
2026-03-25 11:02:44 +0530

    Set up ASP.NET Core application with Swagger support

--- src/Application/Program.cs
+++ src/Application/Program.cs
@@ -1,1 +1,18 @@
+using WorkflowEngineSimulator.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<WorkflowService>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
