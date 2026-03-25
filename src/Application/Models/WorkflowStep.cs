dc6a53c5b7bf267e1067a1a148acf62655ea4e55
ANIKET GHOSH <aniketghosh98@gmail.com>
2026-03-25 11:01:25 +0530

    Update WorkflowStep.cs

--- src/Application/Models/WorkflowStep.cs
+++ src/Application/Models/WorkflowStep.cs
@@ -1,1 +1,11 @@
+namespace WorkflowEngineSimulator.Models;
 
+public class WorkflowStep
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public int Order { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsParallel { get; set; }
+    public StepStatus Status { get; set; } = StepStatus.Pending;
+    public List<ApproverAction> Approvers { get; set; } = new();
+}
